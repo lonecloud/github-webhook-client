e6f2a0cf98e9dc546b2e430745437c1d9b7a557f
lonecloud <lonecloud@aliyun.com>
2021-12-04 01:14:59 +0800

    :zap: update codes

--- src/main/java/com/github/webhook/client/WebHookClient.java
+++ src/main/java/com/github/webhook/client/WebHookClient.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © ${project.inceptionYear} organization lonecloud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.webhook.client;
 
 import com.alibaba.fastjson.JSON;
@@ -21,7 +36,6 @@ import static com.github.webhook.client.cts.GlobalCts.USER_AGENT;
  * WenHookClient
  *
  * @author lonecloud
- * @date 2021/11/29 22:06
  */
 public class WebHookClient {
 
--- src/main/java/com/github/webhook/client/handler/def/CheckHeaderHandler.java
+++ src/main/java/com/github/webhook/client/handler/def/CheckHeaderHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © ${project.inceptionYear} organization lonecloud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.webhook.client.handler.def;
 
 import com.github.webhook.client.WebHookAction;
@@ -16,7 +31,6 @@ import java.security.MessageDigest;
  * 检查Header正确性
  *
  * @author lonecloud
- * @date 2021/11/29 23:09
  */
 @WebHookAction
 public class CheckHeaderHandler implements CheckParamHandler {
